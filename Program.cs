d5e7aecbca7daaafb6f00b731b482049f02ae3ad
IgorDias1998 <igordiass1998@gmail.com>
2025-05-14 11:28:45 -0300

    Adicionando o Login

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using SisONGFront.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("SisONGApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7090/api/");
+});
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SisONGApi"));
+
+//builder.Services.AddScoped<AuthenticationStateProvider, AuthService>();
+//builder.Services.AddScoped<AuthService>();
+//builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
 
