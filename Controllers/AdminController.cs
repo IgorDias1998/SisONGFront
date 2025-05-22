76620052db7c66884765c56f9ab87d152e3bff95
IgorDias1998 <igordiass1998@gmail.com>
2025-05-21 23:58:38 -0300

    Ínicio desenvolvimento da tela de administrador

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SisONGFront.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Voluntarios()
+        {
+            return View();
+        }
+
+        public IActionResult Doadores()
+        {
+            return View();
+        }
+
+        public IActionResult Eventos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Admin/Voluntarios.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SisONGFront.Views.Admin
+{
+    public class VoluntariosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
